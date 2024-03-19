904b159780b743b1e5f0c419e0b2450245f5c22a
“Jonah <etuafuljonah@gmail.com>
2024-03-19 01:01:44 +0000

    Updates

--- src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Title(props) {
-  const styling = props.style + " leading-relaxed ";
-  return <h1 className={styling}>{props.title}</h1>;
-}
-
-export default Title;
--- src/middleware/helper.js
+++
@@ -1,6 +0,0 @@
-// this function to open new tabs
-function handleClick(url) {
-  window.open(url, "_blank");
-}
-
-export default handleClick;
